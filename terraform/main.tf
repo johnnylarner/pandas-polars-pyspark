3fe0eb7b5bac325b4e0c00ac2cb22e76c77fb7b4
james.richardson <james.richardson@alexanderthamm.com>
2023-07-16 22:50:55 +0200

    feat: create s3 bucket with terraform.

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket         = "pandas-polars-pyspark-terraform-state"
+    key            = "default"
+    region         = "eu-west-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "pandas-polars-pyspark"
+  acl    = "private"
+
+  tags = {
+    project        = "ppp"
+  }
+}
