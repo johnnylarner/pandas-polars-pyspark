47676789848d6ada151a16e54c7f76e97e98c410
james.richardson <james.richardson@alexanderthamm.com>
2023-07-22 09:12:46 +0200

    fix: subnet extraction using filter.

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,8 +15,11 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet" "default" {
-  vpc_id     = data.aws_vpc.default.id
+data "aws_subnets" "example" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 module storage {
