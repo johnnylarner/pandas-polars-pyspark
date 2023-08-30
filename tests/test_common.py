341be62563e37f2d69565d9a4b9e56513b480087
chberreth <c.berreth@gmx.de>
2023-08-30 13:53:42 +0200

    commented section -> to be removed

--- tests/test_common.py
+++ tests/test_common.py
@@ -73,3 +73,99 @@ def test_get_schema_polars(polars_df):
     schema = get_schema(polars_df, config)
 
     assert schema == expected_schema
+
+
+# from src.ppp.common import read_parquet_file, read_csv_file
+
+
+# def test_read_zone_lookup_pandas(pandas_api_mock):
+#     config = {
+#         "path": {
+#             "lookup_csv": "zone_lookup.csv"
+#         },
+#         "module": {
+#             "name": "pandas_api"  # Mocked module name for pandas
+#         }
+#     }
+
+#     result = read_zone_lookup(config)
+
+#     assert result == pandas_api_mock.read_csv.return_value
+
+# def test_read_zone_lookup_polars(polars_api_mock):
+#     config = {
+#         "path": {
+#             "lookup_csv": "zone_lookup.csv"
+#         },
+#         "module": {
+#             "name": "polars_api"  # Mocked module name for polars
+#         }
+#     }
+
+#     result = read_zone_lookup(config)
+
+#     assert result == polars_api_mock.read_csv.return_value
+
+
+# def test_read_parquet_pandas(monkeypatch):
+#     # Mock pandas module with a mock read_parquet method
+#     pandas_mock = Mock()
+#     pandas_mock.read_parquet.return_value = "Pandas DataFrame"
+
+#     # Mock importlib.import_module to return the mock pandas module
+#     monkeypatch.setattr(
+#         "scripts.feature_engineering.importlib.import_module", lambda _: pandas_mock
+#     )
+
+#     result = read_parquet_file("path/to/file.parquet", "pandas")
+
+#     assert result == "Pandas DataFrame"
+#     pandas_mock.read_parquet.assert_called_once_with("path/to/file.parquet")
+
+
+# def test_read_parquet_polars(monkeypatch):
+#     # Mock polars module with a mock read_parquet method
+#     polars_mock = Mock()
+#     polars_mock.read_parquet.return_value = "Polars DataFrame"
+
+#     # Mock importlib.import_module to return the mock polars module
+#     monkeypatch.setattr(
+#         "scripts.feature_engineering.importlib.import_module", lambda _: polars_mock
+#     )
+
+#     result = read_parquet_file("path/to/file.parquet", "polars")
+
+#     assert result == "Polars DataFrame"
+#     polars_mock.read_parquet.assert_called_once_with("path/to/file.parquet")
+
+
+# def test_read_csv_pandas(monkeypatch):
+#     # Mock pandas module with a mock read_csv method
+#     pandas_mock = Mock()
+#     pandas_mock.read_csv.return_value = "Pandas DataFrame"
+
+#     # Mock importlib.import_module to return the mock pandas module
+#     monkeypatch.setattr(
+#         "scripts.feature_engineering.importlib.import_module", lambda _: pandas_mock
+#     )
+
+#     result = read_csv_file("path/to/file.csv", "pandas")
+
+#     assert result == "Pandas DataFrame"
+#     pandas_mock.read_csv.assert_called_once_with("path/to/file.csv")
+
+
+# def test_read_csv_polars(monkeypatch):
+#     # Mock polars module with a mock read_csv method
+#     polars_mock = Mock()
+#     polars_mock.read_csv.return_value = "Polars DataFrame"
+
+#     # Mock importlib.import_module to return the mock polars module
+#     monkeypatch.setattr(
+#         "scripts.feature_engineering.importlib.import_module", lambda _: polars_mock
+#     )
+
+#     result = read_csv_file("path/to/file.csv", "polars")
+
+#     assert result == "Polars DataFrame"
+#     polars_mock.read_csv.assert_called_once_with("path/to/file.csv")
